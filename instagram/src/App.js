b42fd0bed1d405fe97a7d10c275701539b5a4a79
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-02-18 16:38:33 +0100

    Tweaking

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
 
 import dummyData from './dummy-data';
-
-console.log(dummyData);
 
 class App extends Component {  
   render() {
