dbdcd9875f23fc67ddbad2470ea89c0f2a503376
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-02-21 14:03:57 +0100

    Converted Login.js to styled-components

---
+++ instagram/src/components/Button.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+    margin: 12px 0 0;
+    font-size: 1.4em;
+    line-height: 1.6em;
+    padding: 0 10%;
+    transition: 0.5s;
+    color: white;
+    background: ${pr => pr.type === 'login' ? `rgba(75, 108, 183, 0.7)` : `initial` };
+    background: ${pr => pr.type === 'login' ? `-webkit-linear-gradient(to right, rgba(24, 40, 72, 0.7), rgba(75, 108, 183, 0.7))` : `initial` };
+    background: ${pr => pr.type === 'login' ? `linear-gradient(to right, rgba(24, 40, 72, 0.7), rgba(75, 108, 183, 0.7))` : `initial` };
+
+    &:hover {
+        transform: ${pr => pr.type === 'login' ? 'scale(1.1)' : 'initial'};
+        background: ${pr => pr.type === 'login' ? 'rgba(75, 108, 183, 0.9)' : 'initial'};
+        background: ${pr => pr.type === 'login' ? 'linear-gradient(to right, rgba(24, 40, 72, 0.9), rgba(75, 108, 183, 0.9))' : 'initial'};
+        background: ${pr => pr.type === 'login' ? '-webkit-linear-gradient(to right, rgba(24, 40, 72, 0.9), rgba(75, 108, 183, 0.9))' : 'initial'};
+    }
+`;
+
+export default Button;
